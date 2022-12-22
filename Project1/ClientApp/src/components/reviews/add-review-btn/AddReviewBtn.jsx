c74e7c02687064c664fd0a3a11b08e4bf8433799
evgeniy-strel <evgeniy.strel@yandex.ru>
2022-12-22 16:51:03 +0500

    Добавление ссылки на AddReviewBtn

--- Project1/ClientApp/src/components/reviews/add-review-btn/AddReviewBtn.jsx
+++ Project1/ClientApp/src/components/reviews/add-review-btn/AddReviewBtn.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './addReviewBtn.scss';
 
 const AddReviewBtn = () => {
   return (
-    <div className="add_review_button">
-      <div className="add_review_text">Добавить отзыв</div>
-      <div className="add_review_icon"></div>
-    </div>
+    <Link to="/add_review" className="add_review_link">
+      <div className="add_review_button">
+        <div className="add_review_text">Добавить отзыв</div>
+        <div className="add_review_icon"></div>
+      </div>
+    </Link>
   );
 };
 
