472ddf58350ffb3f5a4ed423ba472b6cadddb669
evgeniy-strel <evgeniy.strel@yandex.ru>
2023-04-19 12:30:55 +0500

    Добавление dropdown-edit на курс в админке

--- Project1/ClientApp/src/components/admin-panel/course-column-view/CourseColumnView.jsx
+++ Project1/ClientApp/src/components/admin-panel/course-column-view/CourseColumnView.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import { destinyTracks } from '../../../const.ts';
 import './course_column_view.scss';
 import Tracks from '../tracks/Tracks.jsx';
+import { DeleteFilled, EditFilled, FormOutlined } from '@ant-design/icons';
+import { Dropdown } from 'antd';
 
 const CourseColumnView = ({ course, selectedCourse, setSelectedCourse, setSelectedTrack }) => {
   const handleClickArrow = () => {
     setSelectedCourse((prevCourse) => (prevCourse?.id === course?.id ? undefined : course));
   };
+
+  const itemsEdit = [
+    {
+      label: (
+        <>
+          <span className="item-dropdown">Переименовать</span>
+          <EditFilled />
+        </>
+      ),
+      key: '0',
+    },
+    {
+      label: (
+        <>
+          <span className="item-dropdown">Удалить</span>
+          <DeleteFilled />
+        </>
+      ),
+      key: '1',
+    },
+  ];
 
   return (
     <div
@@ -15,7 +38,14 @@ const CourseColumnView = ({ course, selectedCourse, setSelectedCourse, setSelect
       <div className="course_container">
         <div className="course_column" onClick={handleClickArrow}>
           <p className="title">{course.subjectName}</p>
-          <img src="img/arrow-bottom.svg" alt="show tracks" />
+          <div className="icons">
+            <img src="img/arrow-bottom.svg" alt="show tracks" />
+            <div className="dropdown" onClick={(e) => e.stopPropagation()}>
+              <Dropdown menu={{ items: itemsEdit }} placement="bottomRight" trigger={['click']}>
+                <FormOutlined />
+              </Dropdown>
+            </div>
+          </div>
         </div>
         {selectedCourse?.id == course?.id && (
           <Tracks
