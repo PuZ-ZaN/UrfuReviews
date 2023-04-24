ec5e548e742668d1193140cd85befa3c1fc656e3
evgeniy-strel <evgeniy.strel@yandex.ru>
2023-04-24 23:54:07 +0500

    Рефакторинг Review Model.

--- Project1/Models/Review.cs
+++ Project1/Models/Review.cs
@@ -5,27 +5,17 @@ namespace Project1.Models
     public class Review : BaseEntity
     {
         public Guid PrepodId { get; set; }
-        //public string Header { get; set; }
         public string Body { get; set; }
         public List<string> AdditionalImages { get; set; } = new List<string>();
 
-        public Stars Rating { get; set; }
-        public Stars Interest { get; set; }
-        public Stars Benefit { get; set; }
-        public Stars Availability { get; set; }
+        public Int32 Rating { get; set; }
+        public Int32 Interest { get; set; }
+        public Int32 Benefit { get; set; }
+        public Int32 Availability { get; set; }
 
         public bool isMoved { get; set; }
 
         [NotMapped]
         public Prepod? Prepod { get; set; }
-    }
-    public enum Stars
-    {
-        //rare, medium rare, well done и т.д
-        VeryBad,
-        Bad,
-        Middle,
-        Good,
-        VeryGood
     }
 }
