f7bb47029c90a38f893f8391968bab663fa82e3f
evgeniy-strel <evgeniy.strel@yandex.ru>
2023-06-08 20:42:51 +0500

    UI кнопок плохих отзывов в админке

--- Project1/ClientApp/src/pages/admin-panel/admin-bad-reviews/AdminBadReviews.jsx
+++ Project1/ClientApp/src/pages/admin-panel/admin-bad-reviews/AdminBadReviews.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getLimitReviews, getReviews } from './../../../store/selectors';
 import { getBadReviews } from '../../../store/api-actions';
 import Review from './../../../components/reviews/review/Review';
+import { CommentOutlined, DeleteOutlined, UserDeleteOutlined } from '@ant-design/icons';
 
 const AdminBadReviews = () => {
   const dispatch = useDispatch();
@@ -17,9 +18,25 @@ const AdminBadReviews = () => {
   return (
     <div>
       {
-        <div className="reviews">
+        <div className="reviews bad-reviews">
           {reviews.map((review, index) => (
-            <Review key={review.id} teacher={'test'} review={review} />
+            <>
+              <div className="review-top-panel">
+                <div className="delete">
+                  <DeleteOutlined />
+                  Удалить комментарий
+                </div>
+                <div className="get-user-comments">
+                  <CommentOutlined />
+                  Все отзывы пользователя
+                </div>
+                <div className="block-user">
+                  <UserDeleteOutlined />
+                  Заблокировать пользователя
+                </div>
+              </div>
+              <Review key={review.id} teacher={'test'} review={review} />
+            </>
           ))}
         </div>
       }
